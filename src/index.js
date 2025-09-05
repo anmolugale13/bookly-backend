b7cbb808f5fcc763d16cae3dad75c45fd8abed9d
anmolugale13 <anmolugale13@gmail.com>
2025-09-05 22:20:01 +0530

    Update backend CORS config and routes

--- src/index.js
+++ src/index.js
@@ -22,10 +22,11 @@ app.use(
         callback(new Error("Not allowed by CORS"));
       }
     },
-    credentials: true
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
   })
 );
-
 app.use(express.json());
 app.use(morgan("dev"));
 
